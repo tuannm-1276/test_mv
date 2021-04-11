c9ff42ea452e4a0cf2d39e0d94859f97fdbb2bd6
tuannm-1276 <nguyen.minh.tuan-b@sun-asterisk.com>
2021-04-11 19:45:15 +0700

    int base

---
+++ app/src/main/java/com/example/android/test_mv/screen/favorite/FavoriteFragment.kt
@@ -0,0 +1,13 @@
+package com.example.android.test_mv.screen.favorite
+
+import com.example.android.test_mv.R
+import com.example.android.test_mv.base.BaseFragment
+
+class FavoriteFragment: BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_favorite
+
+    override fun initComponents() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/android/test_mv/screen/search/SearchFragment.kt
@@ -0,0 +1,13 @@
+package com.example.android.test_mv.screen.search
+
+import com.example.android.test_mv.R
+import com.example.android.test_mv.base.BaseFragment
+
+class SearchFragment: BaseFragment() {
+    override val layoutId: Int
+        get() = R.layout.fragment_search
+
+    override fun initComponents() {
+        TODO("Not yet implemented")
+    }
+}
